8fafa632fc23e9b4db19f06c759b242eff89613f
Nguyễn Phúc <knkinhu@gmail.com>
2022-08-25 23:30:58 +0700

    t

---
+++ react/src/main/java/com/example/react/ReactApplication.java
@@ -0,0 +1,14 @@
+package com.example.react;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactApplication {
+    @Generated
+    public static void main(String[] args) {
+        SpringApplication.run(ReactApplication.class, args);
+    }
+
+}
